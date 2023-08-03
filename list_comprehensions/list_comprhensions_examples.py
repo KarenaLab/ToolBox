bd932543998505ca95e7cf57f216ba4a4097cb18
Seb Karena <EKChikui@gmail.com>
2023-08-02 21:55:21 -0300

    Update list_comprhensions_examples.py

--- list_comprehensions/list_comprhensions_examples.py
+++ list_comprehensions/list_comprhensions_examples.py
@@ -1,8 +1,10 @@
 # List comprehensions examples [P356]
 
 # Matrix
-matrix = [[i, j] for i in range(0, 3)
-                 for j in range(0, 3)]
+matrix1 = [[i, j] for i in range(0, 3)
+                  for j in range(0, 3)]
+
+matrix2 = [[i for i in range(0, 3)] for j in range(0, 3)]
 
 position = [[i, j, ((4 * i) + j)] for i in range(0, 4)
                                   for j in range(0, 4)]
@@ -19,4 +21,12 @@ square = [(i ** 2) for i in range(0, 10)]
 index = [i for (_, _, i) in position]
 
 
+# Function
+def square_root(x):
+    sqt = x ** (1/2)
+
+    return sqt
+
+
+sqt_list = [square_root(i) for i in range(1, 6)]
 
