11047149f5e9b77516075dc36ee53e3d70fd199a
Seb Karena <EKChikui@gmail.com>
2023-08-03 22:17:07 -0300

    Create color_messages.py

---
+++ colorama/color_messages.py
@@ -0,0 +1,24 @@
+
+import colorama
+from colorama import Fore, Back, Style
+
+# Source
+# https://pypi.org/project/colorama/
+
+
+colorama.init(autoreset=True)
+
+print(Fore.BLUE + "Testing Blue")
+print(Fore.GREEN + "Testing Green")
+print(Fore.CYAN + "Testing Cyan")
+print(Fore.MAGENTA + "Testing Magenta")
+print(Fore.RED + "Testing Red")
+
+print("This is a simple plot")
+print(Fore.WHITE + "Testing WHITE color and comparing with Normal print")
+print(Fore.BLUE + Style.DIM +Back.WHITE + "Testing Blue with dimmer version and WHITE background")
+
+print(Fore.YELLOW + Style.DIM +"Testing Yellow sun NORMAL")
+print(Fore.YELLOW + "Testing Yellow sun NORMAL")
+print(Fore.YELLOW + Style.BRIGHT + "Testing Yellow sun BRIGHT")
+
