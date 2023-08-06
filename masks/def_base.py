14b9a83b628c9416fcdeedb5a5a9fc9e068fffb6
Seb Karena <EKChikui@gmail.com>
2023-08-06 20:43:39 -0300

    Create def_base.py

---
+++ masks/def_base.py
@@ -0,0 +1,26 @@
+# Name [Pxxx]
+
+# Versions
+# 01 - Date - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
