86a685ea9dd92f2f51aab29ebc6b8621108f937a
Seb Karena <EKChikui@gmail.com>
2023-08-06 20:43:46 -0300

    Create file_base.py

---
+++ masks/file_base.py
@@ -0,0 +1,32 @@
+# Name [Pxxx]
+# Short description of the program/module.
+
+
+# Libraries
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+# Functions
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+
+# end
+
