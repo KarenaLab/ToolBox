b4c2f995c75a35bfcfa70263dfa014abc7a55ded
Seb Karena <EKChikui@gmail.com>
2023-09-06 22:59:02 -0300

    Update init_setup_libraries.py

--- setup_libraries/init_setup_libraries.py
+++ setup_libraries/init_setup_libraries.py
@@ -1,11 +1,31 @@
+# Setup - Libraries [P010] ---------------------------------------------
+# Install by script the basic libraries for a Data Scientist
+
+# Versions
+# 01 - Sept 06th, 2023 - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+# 01 - Make it verbose, giving feedback of process.
+
+
+# Libraries
 import sys
 import subprocess
 
 
-# implement pip as a subprocess:
-libraries_list = ["numpy", "pandas", "TensorFlow", "matplotlib", "seaborn",
-                  "pyspark", "scikit-learn", "scipy", "statsmodels", "jupyterlab",
-                  "feature-engine", "xgboost", "lightgbm"]
+# Program --------------------------------------------------------------
+libraries_list = ["numpy", "pandas", "TensorFlow", "matplotlib",
+                  "seaborn", "pyspark", "scikit-learn", "scipy",
+                  "statsmodels", "jupyterlab", "feature-engine",
+                  "xgboost", "lightgbm", "keras", "oauthlib",
+                  "openpyxl"]
 
+
+# Implement **pip** as a subprocess:
 for lib in libraries_list:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', lib])
+
+
+# end
