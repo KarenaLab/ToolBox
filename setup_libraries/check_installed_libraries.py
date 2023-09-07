91faa8e291188568474a7203a0655bfc5981bc88
Seb Karena <EKChikui@gmail.com>
2023-09-06 23:06:52 -0300

    Create check_installed_libraries.py

---
+++ setup_libraries/check_installed_libraries.py
@@ -0,0 +1,23 @@
+# Setup - Libraries [P010] ---------------------------------------------
+# Check installed libraries
+
+# Versions
+# 01 - Sept 06th, 2023 - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+
+# Libraries
+import sys
+import subprocess
+
+
+# Program --------------------------------------------------------------
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split("==")[0] for r in reqs.split()]
+
+
+for i in installed_packages:
+    print(i)
+
