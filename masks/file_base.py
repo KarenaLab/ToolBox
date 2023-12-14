94c68b46f317082f1e898586c932b3388970f2d0
Seb Karena <EKChikui@gmail.com>
2023-12-13 22:16:13 -0300

    Update file_base.py

--- masks/file_base.py
+++ masks/file_base.py
@@ -1,5 +1,5 @@
 # Name [Pxxx]
-# Short description of the program/module.
+# (optional) Short description of the program/module.
 
 
 # Libraries
@@ -9,6 +9,8 @@ import sys
 import numpy as np
 import pandas as pd
 
+import scipy.stats as st
+
 import matplotlib.pyplot as plt
 
 
@@ -17,6 +19,7 @@ sys.path.append(r"c:\python_modules")
 
 
 # Functions
+
 
 
 # Setup/Config
