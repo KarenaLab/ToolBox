8ec22285e75696e972f3a4145d55c753d99d7870
Seb Karena <EKChikui@gmail.com>
2023-12-29 23:32:53 -0300

    Tool for automatic unzip
    Automatic unzip from given (or local) folder, it is important to highlight that does not do recursvive search inside the folders.

---
+++ miscelaneous/unzip_this_local.py
@@ -0,0 +1,55 @@
+# Unzip Folders inside me [P385]
+# Unizips the .zip files at that directory (does not goes for further
+# levels
+
+
+# Libraries
+import os
+import sys
+import shutil
+from zipfile import ZipFile
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+from file_management import *   # files_list,
+#                                 folders_list and,
+#                                 filter_by_extension 
+
+
+# Functions
+def unzip_files(path=None, delete_zip=False):
+    """
+    Unzip all .zip files contained in the current 
+
+    """
+    if(path != None):
+        path_comeback = os.getcwd()
+        os.chdir(path)
+
+    # Read .zip files and folders
+    zip_content = filter_by_extension(files_list(), files_type=[".zip"])
+    folder_content = folders_list()
+
+
+    # Unzip routine
+    for i in zip_content:      
+        name, extension = i.split(".")
+
+        if(folder_content.count(name) == 0):
+            ZipFile(i).extractall(name)
+            print(f" > .zip file extracted: {i}")
+
+        if(delete_zip == True):
+            os.remove(i)
+
+
+    return None
+
+
+# Program --------------------------------------------------------------
+unzip_files()
+
+
+# end
