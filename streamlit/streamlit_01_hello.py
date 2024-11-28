e9f930cd31ab3cf9e66edfb487585fefbbdf1812
Seb Karena <EKChikui@gmail.com>
2024-11-27 21:19:34 -0300

    Create streamlit_01_hello.py

---
+++ streamlit/streamlit_01_hello.py
@@ -0,0 +1,35 @@
+# Name [P446]
+# First steps using the streamlit application
+
+
+# Libraries
+import os
+import sys
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+import streamlit
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+# end
+
