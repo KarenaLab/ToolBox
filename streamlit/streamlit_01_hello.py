edc5c09f0f34abb46ac5c12280b91953a7aef8f9
Seb Karena <EKChikui@gmail.com>
2024-12-06 23:37:07 -0300

    Update streamlit_01_hello.py

--- streamlit/streamlit_01_hello.py
+++ streamlit/streamlit_01_hello.py
@@ -1,8 +1,9 @@
+# [P446] Learning Streamlit basics
 
-import streamlit 
-
+import streamlit
 
 streamlit.write("Hello World")
+
 
 # You should run this command in Windowd PowerShell or comand line
 # >>> streamlit run streamlit_01_hello.py
