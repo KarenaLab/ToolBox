58a0467c176512f81560c36ae333e81724e0af0d
Seb Karena <EKChikui@gmail.com>
2024-12-08 23:06:04 -0300

    Update switch_case.py

--- switch_case/switch_case.py
+++ switch_case/switch_case.py
@@ -1,17 +1,8 @@
-# Name [Pxxx]
-# (optional) Short description of the program/module.
-
+# Name [P447] Python code to emulate switch case
 
 # Libraries
 import os
 import sys
-from collections import namedtuple
-
-import numpy as np
-import pandas as pd
-import scipy.stats as st
-
-import matplotlib.pyplot as plt
 
 
 # Personal modules
@@ -19,7 +10,25 @@ sys.path.append(r"c:\python_modules")
 
 
 # Functions
+def zero():
+    print("You typed zero")
 
+    return None
+
+def sqr():
+    print("n is a perfect square")
+
+    return None
+
+def even():
+    print("n is an even number")
+
+    return None
+
+def prime():
+    print("n is a prime number")
+
+    return None
 
 
 # Setup/Config
@@ -27,9 +36,16 @@ sys.path.append(r"c:\python_modules")
 
 
 # Program --------------------------------------------------------------
+if(__name__ == "__main__"):
+    options = {0: zero,
+               1: sqr,
+               4: sqr,
+               9: sqr,
+               2: even,
+               3: prime,
+               5: prime,
+               7: prime}
 
-
-
+    options[5]()
 
 # end
-
